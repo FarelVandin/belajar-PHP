5eca532213606c2dcc014880e21b9500e65467a4
Farel Vandin <110141343+FarelVandin@users.noreply.github.com>
2022-09-27 15:18:58 +0700

    Add files via upload

---
+++ Login PHP-Tugas Jamkos/welcome.php
@@ -0,0 +1,36 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome </title>
+</head>
+<body>
+    <?php echo "<marquee> <h1>Welcome " . $_SESSION['username'] . "</h1></marquee>"; ?>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    
+</head>
+<body>
+    
+</body>
+</html>
